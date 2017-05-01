93b013cd222e858b3903ccd150f3f618a5fe6236
Dennis Jakob <denjako@web.de>
2017-05-01 16:31:46 +0200

    Dennis: Aufgabe 1 fertig

--- kickoff_aufgabe/kickoff.js
+++ kickoff_aufgabe/kickoff.js
@@ -1,9 +1,14 @@
 var fs = require('fs');
 
-var content;
 fs.readFile("staedte.json",'utf8', function(err,data) {
 if(err){
   throw err;
   }
-  console.log(JSON.stringify(data));
+var JsonArr = JSON.parse(data);
+  for (i in JsonArr.cities){
+      console.log('\n','name: ',JsonArr.cities[i].name
+                  ,'\n','country: ',JsonArr.cities[i].country
+                  ,'\n','population: ',JsonArr.cities[i].population
+                  ,'\n','\n','-------------');
+  }
 });;
