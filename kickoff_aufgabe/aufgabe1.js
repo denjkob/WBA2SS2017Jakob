5dd2c72902be270fa30512d61679841454b1b674
Dennis Jakob <denjako@web.de>
2017-05-07 16:29:24 +0200

    Dennis,Paulo: Code zusammengefügt und leicht überarbeitet

--- kickoff_aufgabe/aufgabe1.js
+++ kickoff_aufgabe/aufgabe1.js
@@ -1,15 +1,10 @@
 var	fs	=	require('fs');
 fs.readFile("staedte.json", "utf8",	function(err,data)	{
-
   if (err) throw err;
 
   var obj = JSON.parse(data);
 
   //Accessing Array Values
-
-  //remember x = myObj.cars[0];
-
-
   for (arraynumber in obj.cities)
   console.log('\n','name: ',obj.cities[arraynumber].name
               ,'\n','country: ',obj.cities[arraynumber].country
