ca337fd4fa98219472bb8d6cc8c21e52d879ae57
Dennis Jakob <denjako@web.de>
2017-07-12 19:57:17 +0200

    Dennis: Dienstgeber vervollständigt auf den momentanen Stand

--- _projekt/dienstnutzer.js
+++ _projekt/dienstnutzer.js
@@ -76,6 +76,18 @@ app.get("/equipment/:id", function(req,res){
   });
 });
 
+app.get("/searchuser/:name", function(req,res){
+  var url = dUrl+req.path;
+  request(url, function (error, response, body) {
+    if(error) res.status(404);
+    console.log("path: ", req.path);
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body);
+    res.send(body); //res.json(body);
+  });
+});
+
 //POST Methoden
 
 app.post("/user", bodyParser.json(),function(req,res){
@@ -121,8 +133,71 @@ app.post("/equipment", bodyParser.json(),function(req,res){
 });
 
 //PUT Methoden
-//TODO
+app.put("/equipment/:id", bodyParser.json(),function(req,res){
+  var url = dUrl+req.path;
+
+  var options = {
+    uri: url,
+    method: "PUT",
+    headers: {
+      "content-type": "application/json"
+    },
+    json: req.body
+  };
+
+  request(options, function(error, response, body){
+    console.log("path: ", req.path);
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body);
+    res.json(body);
+  });
+});
+
+app.put("/user/:id", bodyParser.json(),function(req,res){
+  var url = dUrl+req.path;
+
+  var options = {
+    uri: url,
+    method: "PUT",
+    headers: {
+      "content-type": "application/json"
+    },
+    json: req.body
+  };
+
+  request(options, function(error, response, body){
+    console.log("path: ", req.path);
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body);
+    res.json(body);
+  });
+});
 //DELETE Methoden
+app.delete("/user/:id", function(req,res){
+      var url = dUrl+req.path;
+      request.delete(url, function (error, response, body) {
+        if(error) res.status(400);
+        console.log("path: ", req.path);
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body);
+        res.status(response.statusCode).send(body); //res.json(body);
+      });
+    });
+
+app.delete("/equipment/:id", function(req,res){
+          var url = dUrl+req.path;
+          request.delete(url, function (error, response, body) {
+            if(error) res.status(404);
+            console.log("path: ", req.path);
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body);
+            res.status(response.statusCode).send(body); //res.json(body);
+          });
+        });
 //TODO
 
 app.listen(settings.port, function() {
